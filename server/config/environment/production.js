9354841da53fd891c5a66ec4cb14f754195ed445
vagrant <vagrant@localhost.localdomain>
2016-05-18 14:39:27 +0200

    Built mean from commit (unavailable) on branch (unavailable)

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -16,7 +16,6 @@ module.exports = {
   // MongoDB connection options
   mongo: {
     uri:    process.env.MONGOLAB_URI ||
-            // process.env.MONGOLAB_COPPER_URI ||
             process.env.MONGODB_URI ||
             process.env.MONGOHQ_URL ||
             process.env.OPENSHIFT_MONGODB_DB_URL+process.env.OPENSHIFT_APP_NAME ||
